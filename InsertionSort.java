14d41fe627c2a4001f6e1ccc1a3dd96f9ca811ae
Olaf Westheider <owesthei@msn.com>
2018-06-02 00:04:10 +0000

    Insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,32 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] nums = { -4, -66, 0, 4, 2, 2, 1, 55, 66 };
+
+        showArray(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = i;
+
+            while (j > 0 && (nums[j - 1] > nums[j])) {
+                swap(nums, j, (j -1));
+                j--;
+            }
+        }
+
+        showArray(nums);
+    }
+
+    private static void showArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
